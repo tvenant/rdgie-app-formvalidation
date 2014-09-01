fdee3d2e5f611b229bc4a96d179f666e4f934e1e
john <john@MR0.(none)>
2014-09-01 11:28:54 +0200

    rdgie form validation initial commit

---
+++ src/main/java/com/jverstry/Controller/FormController.java
@@ -0,0 +1,32 @@
+package com.jverstry.Controller;
+
+import javax.validation.Valid;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.jverstry.Data.Subscriber;
+
+@Controller
+public class FormController {
+	
+
+	@RequestMapping(value="/formPage", method=RequestMethod.GET)
+	public String loadFormPage(Model m) {
+		m.addAttribute("subscriber", new Subscriber());
+		return "formPage";
+	}
+	
+	@RequestMapping(value="/formPage", method=RequestMethod.POST)
+	public String submitForm(@Valid Subscriber subscriber, BindingResult result, Model m) {
+		if(result.hasErrors()) {
+			return "formPage";
+		}
+		
+		m.addAttribute("message", "Successfully saved person: " + subscriber.toString());
+		return "formPage";
+	}
+}
